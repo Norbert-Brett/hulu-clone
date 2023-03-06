195c774e97bfb501e9753fa9e944aafcad9172b2
br3tt <roco_norbi@yahoo.com>
2023-03-06 19:17:03 +0000

    latest 💥

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -9,7 +9,7 @@ const Thumbnail= forwardRef(({result},ref) => {
 		ref={ref}
 		className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
 			<img
-			className=" "
+			className=" object-contain rounded-lg"
 				layout="responsive"
 				lazy
 				src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
